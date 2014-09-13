edc66f603625a543a252fcce6afe25e09bdb294b
Rogers Maradiaga <ramcelk@gmail.com>
2014-09-13 06:53:41 -0600

    Commit Inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sipag::Application.config.secret_key_base = 'baa8359e2ea736780474a863a1c08ba9b350d54d920198d272140b94bb84970087cb9b0e69a33b5b7bdfb462680994ddcb9a9090dc8653ec5bc05badc699176b'
